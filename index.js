878c9ba2040ab4deaa9cdd30b5e196954e76743b
kathrin-2 <kathrin@pno.dev>
2022-07-18 08:42:29 -0400

    feat: add start workflow for apis with ddev

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ module.exports = class JimStart {
         // Get path to yarn.lock or package-lock.json file
         const yarnLock = process.cwd() + '/yarn.lock';
         const npmLock = process.cwd() + '/package-lock.json';
+        const ddev = process.cwd() + '/.ddev';
 
         let command;
 
@@ -21,14 +22,19 @@ module.exports = class JimStart {
         try {
             fs.accessSync(yarnLock);
             command = 'yarn dev';
-        } catch(err) {         
-        }
+        } catch(err) {}
         if (!command) {
             try {
                 fs.accessSync(npmLock);
                 command = 'npm run dev';
             } catch(err) {}
         }
+        if (!command) {
+          try {
+              fs.accessSync(ddev);
+              command = 'ddev start';
+          } catch(err) {}
+      }
 
         // Execute command
         const c = spawn(command, [], {shell: true});
